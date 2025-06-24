79b79cff4c0469d782b1a70c95bcf29f5e95050e
Nagendra Kumar <nagendrakumar@Nagendras-MacBook-Pro.local>
2025-06-24 13:46:54 +0530

    setup router

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/nk-31012002/student-api/internal/config"
+import (
+	"github.com/nk-31012002/student-api/internal/config"
+	"net/http"
+)
 
 func main() {
 	//load config
 	cfg := config.MustLoad()
 	//setup database
 	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welome to the student api"))
+	})
 	//setup server
 }
