d3535d7fdc6f912049b690507134428926591973
Nagendra Kumar <nagendrakumar@Nagendras-MacBook-Pro.local>
2025-06-24 13:42:10 +0530

    load config setup

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,10 @@
 package main
 
+import "github.com/nk-31012002/student-api/internal/config"
+
 func main() {
 	//load config
+	cfg := config.MustLoad()
 	//setup database
 	//setup router
 	//setup server
