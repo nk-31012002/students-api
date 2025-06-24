b534824302116c9e7dc2d91656d569e47fdbf5bd
Nagendra Kumar <nagendrakumar@Nagendras-MacBook-Pro.local>
2025-06-24 13:33:41 +0530

    parse config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+)
+
 type HTTPServer struct {
 	Addr string
 }
@@ -9,4 +16,32 @@ type Config struct {
 	Env         string `yaml:"env" env:"ENV" env-required:"true" `
 	StoragePath string `yaml:"env" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flags := flag.String("config", "", "path to configurational file")
+		flag.Parse()
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("configurational file not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("configuration file does not exist %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("error reading configuration file: %s", err.Error())
+	}
+
+	return &cfg
 }
